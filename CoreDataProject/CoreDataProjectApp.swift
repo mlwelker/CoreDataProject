a49ee79e8f9f861addd5212b08f8854939392bd5
mlwelker <nutria.watts.0g@icloud.com>
2023-08-29 16:10:39 -0500

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Michael Welker on 2023-08-29.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
