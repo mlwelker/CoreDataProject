f538b4ab559be4bcc46601f46a30041994b4e453
mlwelker <nutria.watts.0g@icloud.com>
2023-08-30 14:20:26 -0500

    moc merge policy for dupes

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -4,10 +4,25 @@ import SwiftUI
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
     
+    @FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    
     var body: some View {
-        Button("Save") {
-            if moc.hasChanges {
-                try? moc.save()
+        VStack {
+            List(wizards, id: \.self) { wizard in
+                Text(wizard.name ?? "unknown")
+            }
+            
+            Button("Add") {
+                let wizard = Wizard(context: moc)
+                wizard.name = "Harry Potter"
+            }
+            
+            Button("Save") {
+                do {
+                    try moc.save()
+                } catch {
+                    print(error.localizedDescription)
+                }
             }
         }
     }
