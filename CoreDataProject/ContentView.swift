cd39428ed37692db6261be31273bdb09404ca8a4
mlwelker <nutria.watts.0g@icloud.com>
2023-08-29 22:09:01 -0500

    default movie NSManagedObject

--- CoreDataProject/ContentView.swift
+++ CoreDataProject/ContentView.swift
@@ -1,23 +1,9 @@
 
 import SwiftUI
 
-struct Student: Hashable {
-    let name: String
-}
-
 struct ContentView: View {
-    let students = [Student(name: "Harry Potter"), Student(name: "Hermione Granger")]
-    
     var body: some View {
-//        List {
-//            ForEach([2, 4, 6, 8, 10], id: \.self) {
-//                Text("\($0)")
-//            }
-//        }
-        
-        List(students, id: \.self) { student in
-            Text(student.name)
-        }
+        Text("hello world")
     }
 }
 
---
+++ Movie+CoreDataClass.swift
@@ -0,0 +1,8 @@
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
