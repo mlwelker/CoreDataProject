4e1ac78f1fd089b576067216cc1ab8657b48a556
mlwelker <nutria.watts.0g@icloud.com>
2023-08-31 18:11:54 -0500

    FilteredList with generics

--- CoreDataProject/FilteredList.swift
+++ CoreDataProject/FilteredList.swift
@@ -1,17 +1,20 @@
 
+import CoreData
 import SwiftUI
 
-struct FilteredList: View {
-    @FetchRequest var fetchRequest: FetchedResults<Singer>
+struct FilteredList<T: NSManagedObject, Content: View>: View {
+    @FetchRequest var fetchRequest: FetchedResults<T>
+    let content: (T) -> Content
     
     var body: some View {
-        List(fetchRequest, id: \.self) { singer in
-            Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+        List(fetchRequest, id: \.self) { item in
+            self.content(item)
         }
     }
     
-    init(filter: String) {
-        _fetchRequest = FetchRequest<Singer>(sortDescriptors: [], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter))
+    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
+        _fetchRequest = FetchRequest<T>(sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+        self.content = content
     }
 }
 
